0b72ab1743a24df3f5998832598293b364a7ec67
Alan Santos <santos.homeoffice22@gmail.com>
2022-12-05 16:40:16 -0300

    add all

---
+++ Exercício_21-10-22/Exercício1-2.js
@@ -0,0 +1,17 @@
+
+
+
+var membros = ['Os Lutadores – 1', 'Os Lutadores – 2', 
+'Os Lutadores – 3', 'Os Lutadores – 4', 'Os Lutadores – 5'];
+
+var membro1 = membros[0];
+var membro2 = membros[1];
+var membro3 = membros[2];
+var membro4 = membros[3];
+var membro5 = membros[4];
+
+Escreva('Primeiro membro:'+membro1);
+Escreva('Segundo membro:'+membro2);
+Escreva('Terceiro membro:'+membro3);
+Escreva('Quarto membro:'+membro4);
+Escreva('Quinto membro:'+membro5);
